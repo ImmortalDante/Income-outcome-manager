50e6a05c573fe7faa69234dc082e197bf46bcbd9
ImmortalDante <maklakovfdr@gmail.com>
2022-10-01 17:30:31 +0300

    validators added

--- app/models/auth.py
+++ app/models/auth.py
@@ -1,9 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, validator
 
 
 class UserBase(BaseModel):
 	email: str
 	username: str
+
+	@validator("email")
+	def correct_email(cls, email: str) -> str:
+		if "@" not in email.lower():
+			raise ValidationError("Incorrect email value")
+		return email
 
 	class Config:
 		orm_mode = True
@@ -11,6 +17,12 @@ class UserBase(BaseModel):
 
 class UserCreate(UserBase):
 	password: str
+
+	@validator("password")
+	def min_length(cls, password: str) -> str:
+		if len(password) < 5:
+			raise ValidationError("Password should contain at least 5 characters")
+		return password
 
 
 class UserModel(UserBase):
--- app/models/operations.py
+++ app/models/operations.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, ValidationError
 from datetime import date
 from decimal import Decimal
 from app.services.model_enum import OperationKind
@@ -9,6 +9,12 @@ class OperationBase(BaseModel):
 	kind: OperationKind
 	amount: Decimal
 	description: str | None = None
+
+	@validator("amount")
+	def validate_amount(cls, amount: Decimal) -> Decimal:
+		if amount <= 0:
+			raise ValidationError("Amount value can not be less or equal 0")
+		return amount
 
 	class Config:
 		orm_mode = True
